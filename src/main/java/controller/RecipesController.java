24722d1da098c7f67b48cfb644e91757553998ea
Henri Agede <henri.agede@gmail.com>
2024-10-21 17:56:11 +0200

    commit test

--- src/main/java/controller/RecipesController.java
+++ src/main/java/controller/RecipesController.java
@@ -1,8 +1,7 @@
 package controller;
 
 import entity.Ingredient;
-import model.RecipeCarbs;
-import org.apache.catalina.User;
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +16,7 @@ import javax.persistence.Persistence;
 @RestController
 @RequestMapping("/api/recipes")
 public class RecipesController {
-    Logger logger = (Logger) LoggerFactory.getLogger(RecipesController.class);
+    Logger logger = LoggerFactory.getLogger(RecipesController.class);
 
     @RequestMapping(method = RequestMethod.POST)
     public Ingredient addNewIngredient(@RequestBody Ingredient ingredient) {
@@ -25,8 +24,6 @@ public class RecipesController {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("RECIPE_MAKER");
         EntityManager em = emf.createEntityManager();
-
-//        ingredient = em.find(Ingredient.class,"Thon");
 
         em.getTransaction().begin();
         em.persist(ingredient);
